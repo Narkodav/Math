141869ad4e0828e312171ce99e3618c6fc7e7c5a
Narkodav <kolya.shinkarev.111@gmail.com>
2026-07-11 23:57:50 +0300

    removed the build folder from commits

--- build/clang-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Code/C_code/libraries/Math
-# Build directory: E:/Code/C_code/libraries/Math/build/clang-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[ArithmeticsTests]=] "E:/Code/C_code/libraries/Math/build/clang-debug/ArithmeticsTests.exe")
-set_tests_properties([=[ArithmeticsTests]=] PROPERTIES  _BACKTRACE_TRIPLES "E:/Code/C_code/libraries/Math/CMakeLists.txt;66;add_test;E:/Code/C_code/libraries/Math/CMakeLists.txt;0;")
